9acafbce1c7d9060413cee238d843f3a4c9c452c
Angular CLI <angular-cli@angular.io>
2017-06-24 10:46:00 +0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TestISONEPage } from './app.po';
+
+describe('test-isone App', () => {
+  let page: TestISONEPage;
+
+  beforeEach(() => {
+    page = new TestISONEPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
